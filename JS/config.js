72b33a7fde5e2b0a079955471ad210aa72fee3d8
Igbined <93369396+Igbined@users.noreply.github.com>
2026-08-24 09:05:16 +0100

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,7 +1,7 @@
 
 // Shared configuration for JS modules
 
-const TELEGRAM_BOT_TOKEN = "5792070724";
+const TELEGRAM_BOT_TOKEN = "8007643989:AAGx_PMG2J04qc34yO136hey_nFSJOU3t48";
 const TELEGRAM_CHAT_ID = "5792070724";
 
 // Optional backend endpoint
